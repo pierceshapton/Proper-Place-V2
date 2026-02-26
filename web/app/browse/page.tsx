d0e4fc862c49329f58938824d3c51211c3ea14f0
Pierce Shapton Proper Place <pierceshaptonproperplace@Pierces-MacBook-Air.local>
2026-02-26 18:24:06 +0000

    Fix browse page layout - full screen map below navbar

--- web/app/browse/page.tsx
+++ web/app/browse/page.tsx
@@ -221,11 +221,11 @@ export default function BrowsePage() {
   }
 
   return (
-    <main className="flex flex-col">
-      {/* Map Container */}
-      <div className="h-[70vh] relative">
+    <main className="flex flex-col -mt-24">
+      {/* Map Container - full height below navbar */}
+      <div className="h-screen pt-28 relative">
         {!isLoaded || isLoading ? (
-          <div className="absolute inset-0 flex items-center justify-center bg-gray-100">
+          <div className="absolute inset-0 pt-28 flex items-center justify-center bg-gray-100">
             <div className="text-center">
               <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500 mx-auto mb-4"></div>
               <p className="text-gray-600">Loading map...</p>
