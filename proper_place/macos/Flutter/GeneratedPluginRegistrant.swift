e2e622f47c9cf73cab146277839485bd88294ecf
Pierce Shapton Proper Place <pierceshaptonproperplace@Pierces-MacBook-Air-2.local>
2026-03-16 12:44:42 +0000

    Defer push notifications - remove Firebase packages from Flutter for now

--- proper_place/macos/Flutter/GeneratedPluginRegistrant.swift
+++ proper_place/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import firebase_core
-import firebase_messaging
-import flutter_local_notifications
 import flutter_secure_storage_macos
 import geolocator_apple
 import photo_manager
@@ -18,9 +15,6 @@ import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
